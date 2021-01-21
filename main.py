eca2c612cd041d0c39742dd782cdfc157d97f78a
moryoskorot <moryoskorot@gmail.com>
2021-01-21 10:38:52 -0500

    expanding the 'flesh' of the code

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import sys
 # 21-Jan-21
 #####
 
+# for the sake of testing i found setting an alias to 'python main.py' very effective
+
 
 # some functions  i grabbed from the internet
 #
@@ -24,13 +26,34 @@ import sys
 # os.path.isdir('name') checks to see if something is a directory
 
 
-#Reading and printing the
-target_folder = ' '.join(sys.argv[1:])
-print(os.path.isdir(target_folder))
+#  an empty list which will be populated with the data we need to write to the file
+csv_data = []
+
+
+
+# receives one parameter which is the path to proceed on
+def rec_lister(target_path):
+  if( os.path.isdir(target_path)):
+    print("Tomatoe sauce")
 
 
 
 
+#Reading the target folder from the arguments
+target_folder = ' '.join(sys.argv[1:]) #using a ' '.join we make sure to grab a whole folder name or path even if the user forgot to use quotes
+
+if not os.path.isdir(target_folder): #testing to see if received argument is in fact a dir.
+  print("Specified argument is not an existing folder") # if its not , print it out to the user
+  exit() # And exit the program!
+
+# since we know for sure we have a green light to start, we are going to create a csv file.
+csv_file = open("rec_csv.csv", 'w')
 
 
 
+if __name__ == "__main__":
+  rec_lister(target_folder)  
+ 
+  csv_file.writelines(csv_data)
+  csv_file.close()
+  print("Procedure finished")
